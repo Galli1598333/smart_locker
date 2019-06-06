20efeede326d82a2d2515fac403a69c3e470cf6d
Gianluca Capozzi <capozzigianluca96@gmail.com>
2019-06-06 10:08:12 +0200

    Add username to local SignUp

--- exampleFP/app/src/main/java/com/example/fpexample/SignupActivity.java
+++ exampleFP/app/src/main/java/com/example/fpexample/SignupActivity.java
@@ -3,15 +3,25 @@ package com.example.fpexample;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,9 +29,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SignupActivity extends AppCompatActivity {
 
-    private EditText inputEmail, inputPassword;
+    private EditText inputEmail, inputPassword, inputUsername;
     private Button btnSignUp;
     private FirebaseAuth auth;
+
+    // Firebase db
+    private FirebaseFirestore db;
+
+    private final static String TAG = "SignUp";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,18 +50,22 @@ public class SignupActivity extends AppCompatActivity {
         }
         catch (NullPointerException e){}
 
+        db = FirebaseFirestore.getInstance();
+
         auth = FirebaseAuth.getInstance();
 
         btnSignUp = (Button) findViewById(R.id.signUpBtn2);
 
         inputEmail = (EditText) findViewById(R.id.emailEditTxt);
         inputPassword = (EditText) findViewById(R.id.pwdEditTxt);
+        inputUsername = (EditText) findViewById(R.id.idUsernEditText);
 
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = inputEmail.getText().toString().trim();
                 String password = inputPassword.getText().toString().trim();
+                final String username = inputUsername.getText().toString().trim();
 
                 if (TextUtils.isEmpty(email)) {
                     Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
@@ -55,6 +74,11 @@ public class SignupActivity extends AppCompatActivity {
 
                 if (TextUtils.isEmpty(password)) {
                     Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(username)) {
+                    Toast.makeText(getApplicationContext(), "Enter username!", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
@@ -75,8 +99,23 @@ public class SignupActivity extends AppCompatActivity {
                                     Toast.makeText(SignupActivity.this, "Authentication failed." + task.getException(),
                                             Toast.LENGTH_SHORT).show();
                                 } else {
-                                    startActivity(new Intent(SignupActivity.this, MainActivity.class));
-                                    finish();
+                                    final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                                    Log.d(TAG, "email: " + user.getEmail());
+                                    UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                            .setDisplayName(username)
+                                            .build();
+                                    user.updateProfile(profileUpdates)
+                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> task) {
+                                                    if(task.isSuccessful()) {
+                                                        addUser(user);
+                                                        startActivity(new Intent(SignupActivity.this, MainActivity.class));
+                                                        finish();
+                                                    }
+                                                }
+                                            });
+
                                 }
                             }
                         });
@@ -85,4 +124,27 @@ public class SignupActivity extends AppCompatActivity {
         });
 
     }
+
+    // Add user to db
+    private void addUser(FirebaseUser user){
+
+        Map<String, Object> us = new HashMap<>();
+        us.put("username", user.getDisplayName());
+
+        db.collection("users").document(user.getUid())
+                .set(us, SetOptions.merge())
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
+    }
+
 }
